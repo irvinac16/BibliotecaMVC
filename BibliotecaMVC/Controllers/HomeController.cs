d5ed4ba2151247310fa9f1d1710e12054c0e40db
Irvin <irvin.arguera4321@gmail.com>
2026-07-16 17:55:03 -0600

    Agregar archivos de proyecto.

---
+++ BibliotecaMVC/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using BibliotecaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BibliotecaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Libros()
+        {
+            return View();
+        }
+
+        public IActionResult Autores()
+        {
+            return View();
+        }
+
+        public IActionResult Categorias()
+        {
+            return View();
+        }
+
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
+
+        public IActionResult Prestamos()
+        {
+            return View();
+        }
+
+        public IActionResult AcercaDe()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
