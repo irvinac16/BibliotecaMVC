bb1812635e44d1a55df181c061e8db384222c148
Irvin <irvin.arguera4321@gmail.com>
2026-08-20 18:54:12 -0600

    Semana 5

---
+++ BibliotecaMVC/Models/InfoCard.cs
@@ -0,0 +1,8 @@
+﻿namespace BibliotecaMVC.Models
+{
+    public class InfoCard
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ BibliotecaMVC/Models/ServiceCard.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliotecaMVC.Models
+{
+    public class ServiceCard
+    {
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string ButtonText { get; set; }
+    }
+}
